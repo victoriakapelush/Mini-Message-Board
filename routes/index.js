13324347a2fa959e1f03d99c1b060d79043bf775
victoriakapelush <victoriakapelush@gmail.com>
2024-01-07 14:20:57 -0500

    Display array of messages on screen

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini Message Board'});
+});
+
+module.exports = router;
